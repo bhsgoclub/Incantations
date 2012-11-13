523b73f3765bdcb585060cbfe2564190b1008c10
Trent Hensler <thensler@wisc.edu>
2012-11-12 23:09:53 -0600

    Fixes spells.set()

--- Incantations/src/me/bhsgoclub/incantations/Incantations.java
+++ Incantations/src/me/bhsgoclub/incantations/Incantations.java
@@ -162,161 +162,161 @@ public class Incantations extends JavaPlugin
     	spells = new YamlConfiguration.loadConfiguration(spellFile);
     	if (isNew)
     	{
-    		spells.setProperty("Spells.Lightning.Name", "fulmen");
-    		spells.setProperty("Spells.Lightning.CostMultiplier", 1);
-    		spells.setProperty("Spells.Lightning.Master", false);
-    		spells.setProperty("Spells.Lightning.Cooldown", 5);
-    		spells.setProperty("Spells.Lightning.CastItemOnly", false);
+    		spells.set("Spells.Lightning.Name", "fulmen");
+    		spells.set("Spells.Lightning.CostMultiplier", 1);
+    		spells.set("Spells.Lightning.Master", false);
+    		spells.set("Spells.Lightning.Cooldown", 5);
+    		spells.set("Spells.Lightning.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Bubble.Name", "sphaera");
-    		spells.setProperty("Spells.Bubble.CostMultiplier", 1);
-    		spells.setProperty("Spells.Bubble.Master", false);
-    		spells.setProperty("Spells.Bubble.Cooldown", 30);
-    		spells.setProperty("Spells.Bubble.CastItemOnly", false);
+    		spells.set("Spells.Bubble.Name", "sphaera");
+    		spells.set("Spells.Bubble.CostMultiplier", 1);
+    		spells.set("Spells.Bubble.Master", false);
+    		spells.set("Spells.Bubble.Cooldown", 30);
+    		spells.set("Spells.Bubble.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.SlowFall.Name", "tarduscado");
-    		spells.setProperty("Spells.SlowFall.CostMultiplier", 1);
-    		spells.setProperty("Spells.SlowFall.Master", false);
-    		spells.setProperty("Spells.SlowFall.Cooldown", 0);
-    		spells.setProperty("Spells.SlowFall.CastItemOnly", false);
+    		spells.set("Spells.SlowFall.Name", "tarduscado");
+    		spells.set("Spells.SlowFall.CostMultiplier", 1);
+    		spells.set("Spells.SlowFall.Master", false);
+    		spells.set("Spells.SlowFall.Cooldown", 0);
+    		spells.set("Spells.SlowFall.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Heal.Name", "remedium");
-    		spells.setProperty("Spells.Heal.CostMultiplier", 1);
-    		spells.setProperty("Spells.Heal.Master", false);
-    		spells.setProperty("Spells.Heal.Cooldown", 0);
-    		spells.setProperty("Spells.Heal.CastItemOnly", false);
+    		spells.set("Spells.Heal.Name", "remedium");
+    		spells.set("Spells.Heal.CostMultiplier", 1);
+    		spells.set("Spells.Heal.Master", false);
+    		spells.set("Spells.Heal.Cooldown", 0);
+    		spells.set("Spells.Heal.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Blink.Name", "transulto");
-    		spells.setProperty("Spells.Blink.CostMultiplier", 1);
-    		spells.setProperty("Spells.Blink.Master", false);
-    		spells.setProperty("Spells.Blink.Cooldown", 30);
-    		spells.setProperty("Spells.Blink.CastItemOnly", false);
+    		spells.set("Spells.Blink.Name", "transulto");
+    		spells.set("Spells.Blink.CostMultiplier", 1);
+    		spells.set("Spells.Blink.Master", false);
+    		spells.set("Spells.Blink.Cooldown", 30);
+    		spells.set("Spells.Blink.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Breathe.Name", "respiro");
-    		spells.setProperty("Spells.Breathe.CostMultiplier", 1);
-    		spells.setProperty("Spells.Breathe.Master", false);
-    		spells.setProperty("Spells.Breathe.Cooldown", 0);
-    		spells.setProperty("Spells.Breathe.CastItemOnly", false);
+    		spells.set("Spells.Breathe.Name", "respiro");
+    		spells.set("Spells.Breathe.CostMultiplier", 1);
+    		spells.set("Spells.Breathe.Master", false);
+    		spells.set("Spells.Breathe.Cooldown", 0);
+    		spells.set("Spells.Breathe.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Freeze.Name", "frigidus");
-    		spells.setProperty("Spells.Freeze.CostMultiplier", 1);
-    		spells.setProperty("Spells.Freeze.Master", false);
-    		spells.setProperty("Spells.Freeze.Cooldown", 0);
-    		spells.setProperty("Spells.Freeze.CastItemOnly", false);
+    		spells.set("Spells.Freeze.Name", "frigidus");
+    		spells.set("Spells.Freeze.CostMultiplier", 1);
+    		spells.set("Spells.Freeze.Master", false);
+    		spells.set("Spells.Freeze.Cooldown", 0);
+    		spells.set("Spells.Freeze.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Thaw.Name", "concalesco");
-    		spells.setProperty("Spells.Thaw.CostMultiplier", 1);
-    		spells.setProperty("Spells.Thaw.Master", false);
-    		spells.setProperty("Spells.Thaw.Cooldown", 0);
-    		spells.setProperty("Spells.Thaw.CastItemOnly", false);
+    		spells.set("Spells.Thaw.Name", "concalesco");
+    		spells.set("Spells.Thaw.CostMultiplier", 1);
+    		spells.set("Spells.Thaw.Master", false);
+    		spells.set("Spells.Thaw.Cooldown", 0);
+    		spells.set("Spells.Thaw.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Extinguish.Name", "extinguo");
-    		spells.setProperty("Spells.Extinguish.CostMultiplier", 1);
-    		spells.setProperty("Spells.Extinguish.Master", false);
-    		spells.setProperty("Spells.Extinguish.Cooldown", 0);
-    		spells.setProperty("Spells.Extinguish.CastItemOnly", false);
+    		spells.set("Spells.Extinguish.Name", "extinguo");
+    		spells.set("Spells.Extinguish.CostMultiplier", 1);
+    		spells.set("Spells.Extinguish.Master", false);
+    		spells.set("Spells.Extinguish.Cooldown", 0);
+    		spells.set("Spells.Extinguish.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Rain.Name", "pluvia");
-    		spells.setProperty("Spells.Rain.CostMultiplier", 1);
-    		spells.setProperty("Spells.Rain.Master", true);
-    		spells.setProperty("Spells.Rain.Cooldown", 0);
-    		spells.setProperty("Spells.Rain.CastItemOnly", false);
+    		spells.set("Spells.Rain.Name", "pluvia");
+    		spells.set("Spells.Rain.CostMultiplier", 1);
+    		spells.set("Spells.Rain.Master", true);
+    		spells.set("Spells.Rain.Cooldown", 0);
+    		spells.set("Spells.Rain.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Storm.Name", "tempestas");
-    		spells.setProperty("Spells.Storm.CostMultiplier", 1);
-    		spells.setProperty("Spells.Storm.Master", true);
-    		spells.setProperty("Spells.Storm.Cooldown", 0);
-    		spells.setProperty("Spells.Storm.CastItemOnly", false);
+    		spells.set("Spells.Storm.Name", "tempestas");
+    		spells.set("Spells.Storm.CostMultiplier", 1);
+    		spells.set("Spells.Storm.Master", true);
+    		spells.set("Spells.Storm.Cooldown", 0);
+    		spells.set("Spells.Storm.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Clear.Name", "sereno");
-    		spells.setProperty("Spells.Clear.CostMultiplier", 1);
-    		spells.setProperty("Spells.Clear.Master", true);
-    		spells.setProperty("Spells.Clear.Cooldown", 0);
-    		spells.setProperty("Spells.Clear.CastItemOnly", false);
+    		spells.set("Spells.Clear.Name", "sereno");
+    		spells.set("Spells.Clear.CostMultiplier", 1);
+    		spells.set("Spells.Clear.Master", true);
+    		spells.set("Spells.Clear.Cooldown", 0);
+    		spells.set("Spells.Clear.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Mutate.Name", "mutatio");
-    		spells.setProperty("Spells.Mutate.CostMultiplier", 1);
-    		spells.setProperty("Spells.Mutate.Master", false);
-    		spells.setProperty("Spells.Mutate.Cooldown", 0);
-    		spells.setProperty("Spells.Mutate.CastItemOnly", false);
+    		spells.set("Spells.Mutate.Name", "mutatio");
+    		spells.set("Spells.Mutate.CostMultiplier", 1);
+    		spells.set("Spells.Mutate.Master", false);
+    		spells.set("Spells.Mutate.Cooldown", 0);
+    		spells.set("Spells.Mutate.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Wall.Name", "clausus");
-    		spells.setProperty("Spells.Wall.CostMultiplier", 1);
-    		spells.setProperty("Spells.Wall.Master", false);
-    		spells.setProperty("Spells.Wall.Cooldown", 30);
-    		spells.setProperty("Spells.Wall.CastItemOnly", false);
+    		spells.set("Spells.Wall.Name", "clausus");
+    		spells.set("Spells.Wall.CostMultiplier", 1);
+    		spells.set("Spells.Wall.Master", false);
+    		spells.set("Spells.Wall.Cooldown", 30);
+    		spells.set("Spells.Wall.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Replenish.Name", "repleo");
-    		spells.setProperty("Spells.Replenish.CostMultiplier", 1);
-    		spells.setProperty("Spells.Replenish.Master", false);
-    		spells.setProperty("Spells.Replenish.Cooldown", 0);
-    		spells.setProperty("Spells.Replenish.CastItemOnly", false);
+    		spells.set("Spells.Replenish.Name", "repleo");
+    		spells.set("Spells.Replenish.CostMultiplier", 1);
+    		spells.set("Spells.Replenish.Master", false);
+    		spells.set("Spells.Replenish.Cooldown", 0);
+    		spells.set("Spells.Replenish.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Protect.Name", "tueri");
-    		spells.setProperty("Spells.Protect.CostMultiplier", 1);
-    		spells.setProperty("Spells.Protect.Master", false);
-    		spells.setProperty("Spells.Protect.Cooldown", 30);
-    		spells.setProperty("Spells.Protect.CastItemOnly", false);
+    		spells.set("Spells.Protect.Name", "tueri");
+    		spells.set("Spells.Protect.CostMultiplier", 1);
+    		spells.set("Spells.Protect.Master", false);
+    		spells.set("Spells.Protect.Cooldown", 30);
+    		spells.set("Spells.Protect.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Fireball.Name", "ignifera");
-    		spells.setProperty("Spells.Fireball.CostMultiplier", 1);
-    		spells.setProperty("Spells.Fireball.Master", false);
-    		spells.setProperty("Spells.Fireball.Cooldown", 0);
-    		spells.setProperty("Spells.Fireball.CastItemOnly", false);
+    		spells.set("Spells.Fireball.Name", "ignifera");
+    		spells.set("Spells.Fireball.CostMultiplier", 1);
+    		spells.set("Spells.Fireball.Master", false);
+    		spells.set("Spells.Fireball.Cooldown", 0);
+    		spells.set("Spells.Fireball.CastItemOnly", false);
     		
-    		spells).setProperty("Spells.Transmute.Name", "transmutare");
-    		spells.setProperty("Spells.Transmute.CostMultiplier", 0);
-    		spells.setProperty("Spells.Transmute.Master", false);
-    		spells.setProperty("Spells.Transmute.Cooldown", 0);
-    		spells.setProperty("Spells.Transmute.CastItemOnly", false);
+    		spells.set("Spells.Transmute.Name", "transmutare");
+    		spells.set("Spells.Transmute.CostMultiplier", 0);
+    		spells.set("Spells.Transmute.Master", false);
+    		spells.set("Spells.Transmute.Cooldown", 0);
+    		spells.set("Spells.Transmute.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Entomb.Name", "sepulcrum");
-    		spells.setProperty("Spells.Entomb.CostMultiplier", 1);
-    		spells.setProperty("Spells.Entomb.Master", false);
-    		spells.setProperty("Spells.Entomb.Cooldown", 0);
-    		spells.setProperty("Spells.Entomb.CastItemOnly", false);
+    		spells.set("Spells.Entomb.Name", "sepulcrum");
+    		spells.set("Spells.Entomb.CostMultiplier", 1);
+    		spells.set("Spells.Entomb.Master", false);
+    		spells.set("Spells.Entomb.Cooldown", 0);
+    		spells.set("Spells.Entomb.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.GlassToIce.Name", "glacia");
-    		spells.setProperty("Spells.GlassToIce.CostMultiplier", 1);
-    		spells.setProperty("Spells.GlassToIce.Master", false);
-    		spells.setProperty("Spells.GlassToIce.Cooldown", 0);
-    		spells.setProperty("Spells.GlassToIce.CastItemOnly", false);
+    		spells.set("Spells.GlassToIce.Name", "glacia");
+    		spells.set("Spells.GlassToIce.CostMultiplier", 1);
+    		spells.set("Spells.GlassToIce.Master", false);
+    		spells.set("Spells.GlassToIce.Cooldown", 0);
+    		spells.set("Spells.GlassToIce.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Light.Name", "lux");
-    		spells.setProperty("Spells.Light.CostMultiplier", 1);
-    		spells.setProperty("Spells.Light.Master", false);
-    		spells.setProperty("Spells.Light.Cooldown", 0);
-    		spells.setProperty("Spells.Light.CastItemOnly", false);
+    		spells.set("Spells.Light.Name", "lux");
+    		spells.set("Spells.Light.CostMultiplier", 1);
+    		spells.set("Spells.Light.Master", false);
+    		spells.set("Spells.Light.Cooldown", 0);
+    		spells.set("Spells.Light.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.WaterWalking.Name", "superaquas");
-    		spells.setProperty("Spells.WaterWalking.CostMultiplier", 1);
-    		spells.setProperty("Spells.WaterWalking.Master", false);
-    		spells.setProperty("Spells.WaterWalking.Cooldown", 0);
-    		spells.setProperty("Spells.WaterWalking.CastItemOnly", false);
+    		spells.set("Spells.WaterWalking.Name", "superaquas");
+    		spells.set("Spells.WaterWalking.CostMultiplier", 1);
+    		spells.set("Spells.WaterWalking.Master", false);
+    		spells.set("Spells.WaterWalking.Cooldown", 0);
+    		spells.set("Spells.WaterWalking.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Launch.Name", "jaceo");
-    		spells.setProperty("Spells.Launch.CostMultiplier", 1);
-    		spells.setProperty("Spells.Launch.Master", false);
-    		spells.setProperty("Spells.Launch.Cooldown", 5);
-    		spells.setProperty("Spells.Launch.CastItemOnly", false);
+    		spells.set("Spells.Launch.Name", "jaceo");
+    		spells.set("Spells.Launch.CostMultiplier", 1);
+    		spells.set("Spells.Launch.Master", false);
+    		spells.set("Spells.Launch.Cooldown", 5);
+    		spells.set("Spells.Launch.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Break.Name", "ruptura");
-    		spells.setProperty("Spells.Break.CostMultiplier", 1);
-    		spells.setProperty("Spells.Break.Master", false);
-    		spells.setProperty("Spells.Break.Cooldown", 10);
-    		spells.setProperty("Spells.Break.CastItemOnly", false);
+    		spells.set("Spells.Break.Name", "ruptura");
+    		spells.set("Spells.Break.CostMultiplier", 1);
+    		spells.set("Spells.Break.Master", false);
+    		spells.set("Spells.Break.Cooldown", 10);
+    		spells.set("Spells.Break.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Activate.Name", "tincidunt");
-    		spells.setProperty("Spells.Activate.CostMultiplier", 1);
-    		spells.setProperty("Spells.Activate.Master", false);
-    		spells.setProperty("Spells.Activate.Cooldown", 0);
-    		spells.setProperty("Spells.Activate.CastItemOnly", false);
+    		spells.set("Spells.Activate.Name", "tincidunt");
+    		spells.set("Spells.Activate.CostMultiplier", 1);
+    		spells.set("Spells.Activate.Master", false);
+    		spells.set("Spells.Activate.Cooldown", 0);
+    		spells.set("Spells.Activate.CastItemOnly", false);
     		
-    		spells.setProperty("Spells.Silence.Name", "silentium");
-    		spells.setProperty("Spells.Silence.CostMultiplier", 1);
-    		spells.setProperty("Spells.Silence.Master", false);
-    		spells.setProperty("Spells.Silence.Cooldown", 30);
-    		spells.setProperty("Spells.Silence.CastItemOnly", false);
+    		spells.set("Spells.Silence.Name", "silentium");
+    		spells.set("Spells.Silence.CostMultiplier", 1);
+    		spells.set("Spells.Silence.Master", false);
+    		spells.set("Spells.Silence.Cooldown", 30);
+    		spells.set("Spells.Silence.CastItemOnly", false);
     		
     		spells.save();
     	}
